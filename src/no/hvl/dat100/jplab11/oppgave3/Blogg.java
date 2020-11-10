42d0a9e55595616706cd24c9451cf29c55b4246e
orjan <orjan@158.37.224.175>
2020-11-10 13:31:11 +0100

    yessir

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -3,54 +3,100 @@ package no.hvl.dat100.jplab11.oppgave3;
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
-public class Blogg {
+public class Blogg{
 
-	// TODO: objektvariable 
+	private Innlegg[]innleggTabell;
+	private int nesteLedig;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggTabell = new Innlegg[20];
+		nesteLedig = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggTabell = new Innlegg[lengde];
+		nesteLedig = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggTabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		int pos = 0;
+		boolean funnet = false;
+		
+		while(pos < nesteLedig && !funnet) {
+			
+			if(innleggTabell[pos].erLik(innlegg)) {
+				funnet = true;
+			}
+			else pos++;
+		}
+		if(funnet) {
+			return pos;
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+	
+		for(int i = 0; i < nesteLedig; i++) {
+			
+			if(innleggTabell[i].erLik(innlegg)) {
+				return true;
+			}
+		
+		}
+		
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		//for(int i = 0; i < innleggTabell.length; i++) {
+			if(nesteLedig < innleggTabell.length) 
+				return true;
+		//}
+		return false;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		if(ledigPlass() && !(finnes(innlegg))) {
+			innleggTabell[nesteLedig] = innlegg;
+			 nesteLedig++;
+		return true;
+		}		
+		return false;	
 	}
 	
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+	public String toString() { 
+		String utTxt = nesteLedig + "\n";
+		
+		for(int i = 0; i < nesteLedig; i++) {
+			utTxt += innleggTabell[i].toString();
+		}		
+		return utTxt;		
 	}
 
 	// valgfrie oppgaver nedenfor
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] nyTabell = new Innlegg[2*innleggTabell.length];
+		int NesteLedige = 0;
+		
+		for(int i = 0; i < nesteLedig; i++) {
+			nyTabell[i] = innleggTabell[i];
+			NesteLedige++;
+		}
+		
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,43 @@
 package no.hvl.dat100.jplab11.oppgave4;
-
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
+import java.io.PrintWriter.*;
+import java.io.*;
+import java.io.FileNotFoundException.*;
+import static javax.swing.JOptionPane.*;
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+	public static boolean skriv(Blogg samling, String mappe, String filnavn){
+		 boolean ok = true;
+		 FileWriter fw = null;
+		 BufferedWriter bw = null;
+		 PrintWriter pw = null;
+			File fil = new File(mappe + filnavn);
+		try {
+			fw = new FileWriter(fil, false);
+			 bw = new BufferedWriter(fw);		
+			 pw = new PrintWriter(bw);
+			
+			 pw.println(samling.toString());
+			 
+		 }catch(FileNotFoundException e) {
+			ok = false;
+			System.out.println("Fant desverre ikke filen");
+			return ok;
+		}catch(IOException e) {
+			ok = false;
+			System.out.println("IOException");
+		}finally {
+			try {
+				fw.flush();
+				bw.flush();
+				pw.flush();
+			}catch(IOException e) {
+				ok = false;
+				System.out.println("Feilmelding!");
+			}
+		}
+		return ok;
 	}
 }
